0263efc5fe5ea27cad4226c084ffd8f7e2584f01
ihsankyc <kiyici35@gmail.com>
2018-03-02 11:42:32 +0300

    içerikler hazırlandı

--- js/language.js
+++ js/language.js
@@ -5,43 +5,43 @@ var language_tr={
     menu8:"Bize Ulaşın",
 
     //ÜST ALAN
-        tesvik:"Bu Alan İşe Teşvik İle İlgili",
-        tesvik_icerik:"Türkçe Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero ex inventore vel error quibusdam animi fugiat, doloribus dolores consectetur nulla deleniti sint blanditiis quod debitis quis vitae officiis tempora numquam.",
+        tesvik:"BITFINDEX VE BITFINDEX MINING",
+        tesvik_icerik:"COINET GROUP iştirakları olan <a href='http://bitfindex.com' target='_blank'>bitfindex.com</a> ve <a href='http://bitfindex.com' target='_blank'>bitfindexmining.com</a> web sayfalarımızda 7/24 Canlı Destek bulunmaktadır, hızlı ve güvenli işlemlerle sizlere daha iyi hizmet vereceğimizi taahhüt ederiz.",
 
     //ÜST ALAN SON
 
     // HAKKIMIZDA
-        hakkimizda_baslik:"BİZ KİMİZ",
-        hakkimizda_icerik:"Türkçe Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.",
+      //  hakkimizda_baslik:"BİZ KİMİZ",
+      //  hakkimizda_icerik:"Türkçe Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.",
     // VİZYONUMUZ
-        vizyonumuz_baslik:"VİZYONUMUZ",
-        vizyonumuz_icerik:"Türkçe Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem",
+        //vizyonumuz_baslik:"VİZYONUMUZ",
+        //vizyonumuz_icerik:"İlk hedefimiz kendimize ait bulunmakta olan mining tesislerimizi büyütmek,geliştirmek ve <a href='http:\\www.bitfindex.com' target='_blank'>bitfindex.com</a> ile de pazarın nabzını tutabilmektir.Şirketimiz bir yıldan fazla bir süredir mining üzerinde çalışmalar yapmaktadır,ilgilenmektedir. İleride oluşturacağımız havuzlarımız sayesinde sizlere daha iyi hizmet sunabilmeyi hedefliyoruz.",
     // MİSYONUMIUZ
-        misyonumuz_baslik:"MİSYONUMUZ",
-        misyonumuz_icerik:"Türkçe Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem",
+        misyonumuz_baslik:"HAKKIMIZDA",
+        misyonumuz_icerik:"Dijital para birimlerinin global piyasalardan yaygınlaşması üzerine Türkiye piyasalarında işlem görmeye başlamış ve yaygınlaşmıştır. Türkiye piyasasında yer alan dijital para birimleri borsaları dünyadaki diğer ülke borsaları klasmanından çok uzak olup sınırlı sayıda dijital para birimi ile işlem yapmaktadır.COINET LTD. ŞTİ. ülkemizi bu piyasada daha iyi temsil etme yolunda hizmetlerine başlamıştır.Hedefimiz hem piyasalarda güçlü bir borsa olarak yer almak hemde kendi mining(maden) tesislerimizde güçlü üretim havuzları oluşturup hem istihdam hem de ekonomik anlamda ülkemize destek olmaktadır.<br>Hedefimiz,kendimize ait bulunmakta olan mining tesislerimizi büyütmek,geliştirmek ve kendi web sayfamız olan <a href='http:\\bitfindex.com' target='_blank'>bitfindex.com</a> ile de pazarın nabzını tutabilmektir.Şirketimiz bir yıldan fazla bir süredir mining üzerinde çalışmalar yapmaktadır,ilgilenmektedir.İleride oluşturacağımız havuzlarımız sayesinde sizlere daha iyi hizmet sunabilmeyi hedefliyoruz.",
 
     // SON HAKKIMIZDA
     //PROJE
         proje_baslik:"Projelerimiz",
-        proje_icerik:"PROJE İÇERİKLERİ",
+        //proje_icerik:"PROJE İÇERİKLERİ",
 
-        proje1_baslik:"BİTFİNDEX",
-        proje1_icerik:"TR İÇERİK",
+        proje1_baslik:"BITFINDEX",
+        proje1_icerik:"Yatırımlarınız anında hesabınızda olsun!",
     //PROJE SON
 
     // MAİL
     iletisim_baslik:"BİZİMLE İLETİŞİME GEÇİN",
-    iletisim_aciklama:"İletişim Hakkında",
+    iletisim_aciklama:"Bizimle iletişime geçmek için aşağıdaki alanları eksiksiz doldurunuz.<br> En kısa sürede sizlere dönüş yapılacaktır.",
     buton:"Gönder",
     adress:"Adres:",
     telephone:"Telefon:",
-    acik_adres:"Açık adres",
+    acik_adres:"Tacettin Veli Mahallesi Delikli Taş Caddesi Nish Plaza 20/17 Melihgazi/Kayseri/TÜRKİYE",
 
     mail:{
-        isim:"Adınız:",
-        email_adres:"E-mail adresiniz:",
-        mesaj_konusu:"Mesaj konusu:",
-        mesaj:"Mesajınız:",
+        isim:"Adınız",
+        email_adres:"E-mail adresiniz",
+        mesaj_konusu:"Mesaj konusu",
+        mesaj:"Mesajınız",
         basarili_mail_mesaji:"Mailiniz Gönderilmiştir.",
         hatali_giris:"Lütfen İstenilen Bilgileri Doğru Bir Şekilde Doldurunuz!",
     }
@@ -56,37 +56,37 @@ var language_en={
     menu5:"Projects",
     menu8:"Contact us",
     //ÜST ALAN
-    tesvik:"It’s all about Promoting your Business",
-    tesvik_icerik:"English Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero ex inventore vel error quibusdam animi fugiat, doloribus dolores consectetur nulla deleniti sint blanditiis quod debitis quis vitae officiis tempora numquam.",
+    tesvik:"BITFINDEX AND BITFINDEX MINING",
+    tesvik_icerik:"COINET GROUP participation <a href='http://www.bitfindex.com' target='_blank'>bitfindex.com</a> and <a href='http://www.bitfindex.com' target='_blank'>bitfindexmining.com</a> web pages have 24/7 Live Support, we promise to serve you better with fast and safe proccess.",
     //ÜST ALAN SON
 
     // HAKKIMIZDA
-        hakkimizda_baslik:"WHO WE ARE?",
-        hakkimizda_icerik:"English Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.",
+      //  hakkimizda_baslik:"WHO WE ARE?",
+       // hakkimizda_icerik:"English Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore.",
     // VİZYONUMUZ
-        vizyonumuz_baslik:"Our vision",
-        vizyonumuz_icerik:"English Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem",
+      //  vizyonumuz_baslik:"Our vision",
+      //  vizyonumuz_icerik:"Our first vision is to enhance our mining facilities to improve ourselves, to develop and to market with <a href='http:\\www.bitfindex.com' target='_blank'>bitfindex.com</a> A company is engaged in more than a decade of mining activities. We aim to provide better service to our pools in the future.",
     // MİSYONUMIUZ
-        misyonumuz_baslik:"Our mission",
-        misyonumuz_icerik:"English Lorem ipsum dolor sit amt, consectet adop adipisicing elit, sed do eiusmod tepo raraincididunt ugt labore. Lorem",
+        misyonumuz_baslik:"ABOUT US",
+        misyonumuz_icerik:"The spread of digital currency on the global markets began to see the market transactions in Turkey and has become widespread. Digital earth in Turkey market currencies and other countries in the world stock markets division in multi-currency transactions or limited yapmaktadır.coınet LTD. Sti. has started to serve our country in the way of better representation in this market. Our goal is to be a powerful stock exchange in the market and also to support our country in both employment and economic sense, creating powerful production pools in our own mining (mining) facilities.<br>Our goal is to enlarge our mining facilities that belong to ourselves, and to improve our market's pulse with our own web page BitFindex.com Our company is engaged in mining for more than a year.Our vision is to provide better service with our pools in the future.",
 
     // SON HAKKIMIZDA
 
     //PROJELER
         proje_baslik:"Projects",
-        proje_icerik:"ABOUT PROJECTS....",
+      //  proje_icerik:"ABOUT PROJECTS....",
 
-         proje1_baslik:"BİTFİNDEX",
-        proje1_icerik:"EN İÇERİK",
+         proje1_baslik:"BITFINDEX",
+        proje1_icerik:"Your investments are instantly on your account",
     //PROJE SON
 
      // MAİL
     iletisim_baslik:"CONTACT US",
-    iletisim_aciklama:"Communication",
+    iletisim_aciklama:"Please fill the fields below to communicate with us.<br>You will be answered shortly.",
     buton:"Submit",
     adress:"Adress:",
     telephone:"Telephone:",
-    acik_adres:"Adress Information",
+    acik_adres:"Tacettin Veli Mahallesi Delikli Taş Caddesi Nish Plaza 20/17 Melihgazi/Kayseri/TURKEY",
 
 
     mail:{
